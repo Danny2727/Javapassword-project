2b888c08105456ccc55f8abfd7c51b68b9839f34
Meech <meech@dannys-mbp.attlocal.net>
2023-01-19 14:21:07 -0500

    Made changes to the functions declarations and change some of the varibales names

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Javapassword-project
+# JAVA-Password
+
+## Motivation
+
+-The purpose of this project was to create a application that would generate a random password depending on the criteria that was selected by the user.
+I had to us a mixture of Arrays and if/else conditional statments to get the program to fucntion as intended.
+
+## Installation
+-Clone the repo
+-git init
+-git pull origin main
+-git push 
+
+## Screenshot
+-
+
+## Website Link
+-
+
+## Usage
+Use the file work on java script skills. Feel free to change/ modify the password id genreated.
+
+License
+MIT License
+
+Copyright (c) [2023 [Daniel Edouard]
+
+Feel free to make any changes to the file without any restrictions.x
--- script.js
+++ script.js
@@ -1,82 +1,62 @@
 // Assignment code here
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Z"]
-var numberVal = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "{", "}"]
+// The Array of upper case letters, lower case letters, numbers, and special characters.
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Z"]
+var numberValArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var specCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "{", "}"]
 var userChoice = [];
-
-var passwordComplete;
-
+var length = "";
 
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+// This function contains the criteria that must be met to generate a password.
 function generatePassword() {
-    // 1.Prompt User for password Criteria
-    //      a. Password length between 8 < 128
-    //      b.Lowercase, uppercase, numbers, special characters
-    //2.Validate input
-    var option = prompt("Choose a password between 8 and 128 characters");
-    if (length < 8 || length > 128){
-      }  else {
-        return "password must be between 8 - 128 characters long";
+    length = prompt("Choose a password between 8 and 128 characters");
+    if (length > 8 && length < 128); {
+
+        if (confirm("Would you like to use uppercase letters.")) {
+            userchoice = userChoice.concat(upperCaseArray);
+        }
+
+        if (confirm("Would you like to use lowercase letters.")) {
+            userChoice = userChoice.concat(lowerCaseArray);
+        } 
+        if (confirm("Would you like to inlude numbers.")) {
+            userChoice = userChoice.concat(numberValArray);
+        } 
+        if (confirm("Would you like to include special characters.")) {
+            userChoice = userChoice.concat(specCharArray);
+        }
+        else {
+            console.log("User must select password length and at least one of the password criterias");
+        }
+        
     }
 
-    var upperCase = confirm("Would you like to use uppercase letters.")
-    if (confirm("Would you like to use uppercase letters.") === true) {
-        userChoice.concat(upperCase);
-    } else {
-        console.log("not selected");
+}
+
+function createPassword() {
+    var selectedPassword = "";
+    for (var i = 0; i < length; i++) {
+        var finishedPassword = Math.floor(Math.random() * [userChoice.length]);
+        password = password + userChoice[finishedPassword];
     }
-
-    var lowerCase = confirm("Would you like to use lowercase letters.")
-    if (lowerCase("Would you like to use lowercase letters.") === true) {
-        userChoice.concat(lowerCase);
-    } else {
-        console.log("not selected");
-    }
-
-    var numberVal = confirm("Would you like to inlude numbers.")
-    if (confirm("Would you like to incldue numbers.") === true) {
-        userChoice.concat(numVal);
-    } else {
-        console.log("not selected");
-    }
-
-    var specChar = confirm("Would you like to include special characters.")
-    if (confirm("Would you like to include special characters.") === true) {
-        userChoice.concat(specChar);
-    } else {
-        console.log("not selected");
-    }
-
-
-
-    for (let i = 0; i < passwordLength; i++) {
-        password = Math.floor(Math.random() * userChoice.length);
-    }
-
-
-
-
-    //3.Generate password based on Criteria
-
-    //4.Display Generated password on page.
-    return "Generated password will display here"
-
 }
 
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
+    var passordNew = createPassword();
+    if (password) {}
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
